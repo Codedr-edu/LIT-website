30a26586870d0de30d0bdf5395322516e0fdd99a
Hoang Hung Anh <90163072+Codedr-edu@users.noreply.github.com>
2025-07-19 13:05:04 +0700

    Add files via upload

--- lit_web/urls.py
+++ lit_web/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lit.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('froala_editor/', include('froala_editor.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
